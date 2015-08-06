23eaab4e8c828f41db4dd69c2e17ebe938650261
Soo Jin Oh <soo.jin.o@gmail.com>
2015-08-06 12:43:58 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('#cat1').click(function(e) {
+ 	var count =Number($('#count1').html())+1;
+ 	$('#count1').html(count);
+});
+$('#cat2').click(function(e) {
+ 	var count =Number($('#count2').html())+1;
+ 	$('#count2').html(count);
+});
